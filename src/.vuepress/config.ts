cbec7b274dd67c8c34f46f75416f45a2b676b563
Jiwan ghosal <jiwan.cse@gmail.com>
2024-12-03 09:03:30 +0530

    feat: Analytics

--- src/.vuepress/config.ts
+++ src/.vuepress/config.ts
@@ -23,7 +23,19 @@ export default defineUserConfig({
     vuePluginOptions: {},
   }),
   base: "/",
+  head: [
+    ['script', {
+        src: 'https://www.googletagmanager.com/gtag/js?id=G-2H3QT4CP1Y', 
+        async: true,
+    }],
+    ['script', {}, `
+      window.dataLayer = window.dataLayer || [];
+      function gtag(){dataLayer.push(arguments);}
+      gtag('js', new Date());
 
+      gtag('config', 'G-2H3QT4CP1Y');
+  `]
+  ],
   locales: {
     "/": {
       lang: "en-US",
